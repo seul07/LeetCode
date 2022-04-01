33c712caed749d2790bce450127b7e5075a33526
seul07 <75015063+seul07@users.noreply.github.com>
2022-04-01 14:10:29 +0900

    Time: 1 ms (84.55%), Space: 57.7 MB (27.08%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,18 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        int lf = 0;
+       int lf = 0;
         int rf = s.length-1;
         while(lf<rf){
-            char lfTmp = s[lf];
-            char rfTmp = s[rf];
-            char tmp = ' ';
-            tmp = rfTmp;
-            s[rf] = lfTmp;
+            char tmp = s[rf];
+            s[rf] = s[lf];
             s[lf] = tmp;
             lf++;
             rf--;
         }
-
-        System.out.println(Arrays.toString(s));
     }
 }
