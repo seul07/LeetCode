52ced174d3c9b8e5b675efe377f8f2d5b4660947
seul07 <75015063+seul07@users.noreply.github.com>
2022-04-07 16:48:49 +0900

    Time: 2 ms (76.58%), Space: 42.5 MB (5.22%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,17 +1,17 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-         List<Integer> list= Arrays.stream(stones).boxed().collect(Collectors.toList());
-        list.sort(Comparator.reverseOrder());
-        int result = stones[0];
-        while(list.size()>1){
-            int max = list.get(0);
-            int secMax = list.get(1);
-            list.add(max-secMax);
-            list.remove(0);
-            list.remove(0);
-            list.sort(Comparator.reverseOrder());
-            result = list.get(0);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int n : stones){
+            priorityQueue.add(n);
         }
+        int result = stones[0];
+        while(priorityQueue.size()>1){
+            int max = priorityQueue.poll();
+            int secMax = priorityQueue.poll();
+            priorityQueue.add(max-secMax);
+            result = priorityQueue.peek();
+        }
+
         return result;
     }
 }
