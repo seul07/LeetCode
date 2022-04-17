c3a5bd4a4be72bc28d91ee927e9daf58023e4644
sseul0701@argonet.co.kr <0917shfkd*>
2022-04-17 20:34:50 +0900

    commit from mac

---
+++ src/test/java/com/example/leetcode/BST.java
@@ -0,0 +1,24 @@
+package com.example.leetcode;
+
+import javax.swing.tree.TreeNode;
+
+public class BST {
+
+
+//    public void test(){
+//        fakeRoot = new TreeNode(0);
+//        TreeNode ans = fakeRoot;
+//        inorder(root);
+//        return ans.right;
+//    }
+//
+//    private void inorder(TreeNode t) {
+//        if (t != null) {
+//            inorder(t.left);
+//            fakeRoot.right = t;
+//            fakeRoot = fakeRoot.right;
+//            t.left = null;
+//            inorder(t.right);
+//        }
+//    }
+}
